80c0e39764692d6044a91f968b2a275bb25e6d81
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2024-08-18 14:55:11 +0530

    18: some other fixes

--- config/middleware.js
+++ config/middleware.js
@@ -1,14 +1,15 @@
 const passport = require('passport');
+const { error } = require('winston');
 
 const authMiddleware = (req, res, next) => {
   passport.authenticate('jwt', { session: false }, (err, user) => {
     if (err) {
-      console.error(err.message);
+      // console.error(err.message);
       return next();
     }
     if (!user) {
       console.error('Unauthorized');
-      res.status(401).json({ message: 'Unauthorized' });
+      res.status(401).json({ error: 'Unauthorized' });
     }
     req.user = user;
     next();
